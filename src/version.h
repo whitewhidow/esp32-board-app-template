a1fb77a209534c82f0c62baf14883e3d586a4e83
whitewhidow <contact@branchbit.be>
2026-09-06 10:45:06 +0200

    Release v0.1.5

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
 #  endif
 #endif
 
-#define APP_VERSION "0.1.4"
+#define APP_VERSION "0.1.5"
 
 // Your app's name — the BLE advertised name + shown on the display. The portal /
 // flasher / landing pages carry their own APP_NAME const (top of each HTML) since
